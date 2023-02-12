dcb3239436ad4bea81e457575b5940502b47d2ce
Mr. Sup3rN0va <m2sup3rn0va@gmail.com>
2023-02-13 00:41:06 +0530

    First Sync

--- ggt/network/nmap.sh
+++ ggt/network/nmap.sh
@@ -5,9 +5,8 @@ tput reset clear
 
 printf "[+] Initiating Installation...\n" | lolcat
 
-sudo apt-get install -y git libssl-dev python3 python3-pip python3-distutils python3-apt
-git clone https://github.com/nmap/nmap.git
-temp=$PWD && cd nmap && ./configure && make && sudo make install && sudo ./nmap --script-updatedb
+sudo apt-get install -y nmap
+sudo nmap --script-updatedb
 
 if [ ! -f $HOME/.bash_profile ];
 then
@@ -19,8 +18,6 @@ then
 		printf "alias nsescripts='ll /usr/local/share/nmap/scripts'" >> $HOME/.zshrc
 	fi
 fi
-
-cd $temp && rm -rf nmap
 
 wget -q https://raw.githubusercontent.com/mubix/tools/master/nmap/scripts/smb-check-vulns.nse
 sudo mv -f smb-check-vulns.nse /usr/local/share/nmap/scripts/
