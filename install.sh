b83a43bdc284884746ba6d4ada07d0dcc2e9e83b
Mr. Sup3rN0va <m2sup3rn0va@gmail.com>
2023-02-15 10:03:37 +0530

    Updated-gg-15Feb2023

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ fi
 sudo apt-get clean
 tput reset clear
 
-printf "[+] Initiating Installation...\n" | lolcat
-
 if [ ! -d $HOME/.gg ];
 then
+	printf "[+] Initiating Installation...\n" | lolcat
+
 	mkdir -p $HOME/.gg/update-cache
 	touch $HOME/.gg/update-cache/gg_network.list
 	touch $HOME/.gg/update-cache/gg_web.list
@@ -26,8 +26,18 @@ then
 	printf "[+] Type 'gg' or 'gg help' to get going...\n" | lolcat
 	printf "[+] Type 'gg version' to get the version date...\n" | lolcat
 else
-	printf "\n[+] GitGun(gg) is already installed...\n" | lolcat
+	printf "[+] GITGUN(gg) - THE TOOLKIT MANAGER\n" | lolcat
+	echo -e "------------------------------------\n" | lolcat
+
+	printf "[+] Updating GitGun(gg)\n" | lolcat
+	wget -q https://raw.githubusercontent.com/m2sup3rn0va/gg/main/gg -O $HOME/.gg/gg
+	wget -q https://raw.githubusercontent.com/m2sup3rn0va/gg/main/gg.list -O $HOME/.gg/gg.list
+	chmod +x $HOME/.gg/gg
+	sudo ln -sf $HOME/.gg/gg /usr/bin/gg
+
+	printf "\n[+] GitGun(gg) has been updated successfully...\n" | lolcat
 	printf "[+] Type 'gg' or 'gg help' to get going...\n\n" | lolcat
+	printf "[+] Type 'gg version' to get the version date\n\n" | lolcat
 
 	cat $HOME/.gg/gg.list | grep 'NOTE:' | lolcat
 	echo ""
